309b66b58a43cce05afa9e5c9e8ecae85e5fd315
Tony Newton <atnewton@gmail.com>
2018-10-14 09:57:12 +0100

    Caeser cipher working but spaces need an exception

--- Caesar/Program.fs
+++ Caesar/Program.fs
@@ -2,7 +2,27 @@
 
 open System
 
+let shift(c, shiftAmount) = 
+     let num = int(c) - int('A')
+     let offsetNum = (num+shiftAmount)%26
+     
+     if offsetNum < 0 then
+        char(int('Z') + offsetNum + 1)
+     else
+        char(offsetNum + int('A'))
+
+let shiftstring(str:string, shiftAmount) =
+    str.ToCharArray() 
+    |> Array.map (fun c -> shift( int(c), shiftAmount)) 
+    |> String.Concat
+
+let encrypted = "ocz hvomds dn v ntnozh izj ocvo ntnozh dn jzm zizht".ToUpper()
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    Console.ForegroundColor <- ConsoleColor.Green
+    let decrypted = shiftstring(encrypted, -21)
+    Console.WriteLine(decrypted)
+    System.Console.ReadKey() |> ignore
     0 // return an integer exit code
+
---
+++ Substitution/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    Console.ForegroundColor <- ConsoleColor.Green
+    printfn "Hello World from F#!"
+    System.Console.ReadKey() |> ignore
+    0 // return an integer exit code
